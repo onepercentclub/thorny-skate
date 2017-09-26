6ca46bc2a3731ff2e61d636d57db700b13f55028
Maurice Kleine <info@mauricekleine.nl>
2017-09-26 12:14:36 +0200

    project page

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,18 +1,47 @@
 <template>
-  <div>
-    We show a project overview here
+  <div v-if="project">
+    <h1>
+      {{project.title}}
+    </h1>
+
+    <p v-html="project.pitch"></p>
+
+    <!-- Make a chart / diagram of this -->
+    <h3>
+      €{{project.amount_donated.amount}} of €{{project.amount_asked.amount}} donated by {{project.supporter_count}} supporters!
+    </h3>
+
+    <router-link to="/">Donate!</router-link>
   </div>
 </template>
 
 <script>
+import { getProject } from '@/api';
+
 export default {
+  beforeDestroy() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  },
+  created() {
+    this.getProject();
+
+    // Get the latest data every 30 seconds
+    this.interval = setInterval(this.getProject, 30000);
+  },
   data() {
     return {
-
+      project: null,
+      interval: null,
     };
   },
   methods: {
-
+    getProject() {
+      getProject().then((project) => {
+        this.project = project;
+      });
+    },
   },
   name: 'project',
 };
