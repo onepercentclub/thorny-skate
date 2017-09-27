fb833fcb73bc3d9c7c8b2efea96f85f8b331d4d2
Pieter-Rees <pieter@pieterrees.nl>
2017-09-27 15:18:49 +0200

    remove uikit

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
-import '../node_modules/uikit/dist/css/uikit.css';
 import store from './store';
 
 Vue.config.productionTip = false;
