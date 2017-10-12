5d789c2e36555ba2bec9ac34bd1a2618ce73bce6
Maurice Kleine <info@mauricekleine.nl>
2017-10-12 17:15:20 +0200

    fix url

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.NODE_ENV === 'production'
-    ? 'https://starry-ray.dokku.onepercentclub.com'
+    ? 'https://starry-ray.dokku.onepercentclub.com/api'
     : 'http://localhost:3000/api',
   withCredentials: 'include',
 });
